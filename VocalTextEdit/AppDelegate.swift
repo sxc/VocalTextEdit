8ec70687c104c560669c4436f4e70b5cfbc88674
Shen Xiaochun <shendeveloper@gmail.com>
2017-03-16 15:12:34 +0800

    Initial Commit

---
+++ VocalTextEdit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VocalTextEdit
+//
+//  Created by Shen Xiaochun on 2017/3/16.
+//  Copyright © 2017年 Sigutian. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VocalTextEdit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VocalTextEdit
+//
+//  Created by Shen Xiaochun on 2017/3/16.
+//  Copyright © 2017年 Sigutian. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
