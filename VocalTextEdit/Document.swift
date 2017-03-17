b229203e81732dd3c89eae63aca119a372df336e
Shen Xiaochun <shendeveloper@gmail.com>
2017-03-17 09:49:06 +0800

    add Save the file

--- VocalTextEdit/Document.swift
+++ VocalTextEdit/Document.swift
@@ -9,12 +9,23 @@
 import Cocoa
 
 class Document: NSDocument {
-
-    override init() {
-        super.init()
-        // Add your subclass-specific initialization here.
+    
+    enum Error: Swift.Error, LocalizedError {
+        case UTF8Encoding
+        case UTF8Decoding
+        
+        var failureReason: String? {
+            switch self {
+            case .UTF8Encoding: return "File cannot be encoded in UTF-8."
+            case .UTF8Decoding: return "File is not valid UTF-8."
+            }
+        }
     }
+    
 
+
+    var contents: String = ""
+    
     override class func autosavesInPlace() -> Bool {
         return true
     }
@@ -27,17 +38,29 @@ class Document: NSDocument {
     }
 
     override func data(ofType typeName: String) throws -> Data {
-        // Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
-        // You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+
+        
+        let windowController = windowControllers[0]
+        let viewController = windowController.contentViewController as! ViewController
+        let contents = viewController.textView.string ?? ""
+        
+        guard let data = contents.data(using: .utf8) else {
+            throw Document.Error.UTF8Encoding
+        }
+        return data
+    
+    
+
     }
 
     override func read(from data: Data, ofType typeName: String) throws {
-        // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
-        // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
-        // If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
-    }
+
+        let windowController = windowControllers[0]
+        let viewController = windowController.contentViewController as! ViewController
+        
+           }
+    
+    
 
 
 }
--- VocalTextEdit/ViewController.swift
+++ VocalTextEdit/ViewController.swift
@@ -10,14 +10,25 @@ import Cocoa
 
 class ViewController: NSViewController {
     
+    let speechSynthesizer = NSSpeechSynthesizer()
+    
     @IBOutlet var textView: NSTextView!
     
     @IBAction func speakButtonClicked(_ sender: NSButton) {
-        print("The speak button was clicked")
+//        print("The speak button was clicked")
+//        print("I should speak \(textView.string)")
+        if let contents = textView.string, !contents.isEmpty {
+            speechSynthesizer.startSpeaking(contents)
+        } else {
+            speechSynthesizer.startSpeaking("The document is empty.")
+        }
+
+    
     }
     
     @IBAction func stopButtonClicked(_ sender: NSButton) {
-        print("The stop button was clicked")
+//        print("The stop button was clicked")
+        speechSynthesizer.stopSpeaking()
     }
 
     
